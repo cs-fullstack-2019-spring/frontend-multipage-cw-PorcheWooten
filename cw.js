be70c411798e09b49dea5e4407428ab47033b899
PorcheWooten <porchewooten@yahoo.com>
2019-02-07 16:36:49 -0600

    graded

--- cw.js
+++ cw.js
@@ -1,6 +1,20 @@
-var tab = document.getElementById("home");
+// allow tabs to highlight black
+var tab1 = document.getElementById("home");
+var tab2= document.getElementById("about");
+var tab3= document.getElementById("projects");
+var tab4= document.getElementById("contact");
 
-tab.addEventListener("click", function (e) {
-    tab
-})
-}
+
+tab1.addEventListener("click", function (e) {
+    tab1.style.background= "black";
+
+});
+tab2.addEventListener("click", function (e) {
+    tab2.style.background= "black";
+});
+tab3.addEventListener("click", function (e) {
+    tab3.style.background= "black";
+});
+tab4.addEventListener("click", function (e) {
+    tab4.style.background= "black";
+});
